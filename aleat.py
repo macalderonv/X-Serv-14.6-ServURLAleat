67cb6b9a1ccf47381a298ea08707cdb8102f6889
macalderonv <calderonv94@gmail.com>
2018-03-05 19:16:09 +0100

    v3 aleat.py

--- aleat.py
+++ aleat.py
@@ -4,13 +4,12 @@
 import webapp
 import random
 
-class urls_aleatorias(webapp.webApp):
-	"""docstring for urlsaleat"""
 
-    def process(self, parsedRequest):
-        numero = random.randint(1, 1000000)
-        url = "http://localhost:1234/" + str(numero)
-        return ("200 OK", "<html><body><h1> Hola <a href=" + url + ">dame otra URL</a></    h1></body></html>")
+class urlsaleat(webapp.webApp):
+	"""docstring for urlsaleat"""
+	def process (self, parsedRequest):
+		num_aleat = random.randrange(1000000)
+		return ("200 OK", "<html><body><h1> Hola <a href=" + str(num_aleat) + ">dame otra URL</a></h1></body></html>")
 
 if __name__ == "__main__":
-    testWebAppUrls = urls_aleatorias("localhost", 1234)
+    testWebApp = urlsaleat("localhost", 1234)
